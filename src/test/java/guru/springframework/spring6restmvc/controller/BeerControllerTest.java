e8aaedb1e1b401f1644928bb9a5c52a46e7878c7
Riccardo Corsi <riccardogcorsi@gmail.com>
2025-09-12 17:57:56 +0200

    @RequestMapping listBeers

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,11 +1,13 @@
 package guru.springframework.spring6restmvc.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import guru.springframework.spring6restmvc.model.Beer;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -18,6 +20,13 @@ public class BeerControllerTest {
     @Test
     void testGetBeerByID() {
         log.debug("Test getBeerByID");
-        System.out.println(beerController.getBeerByID(UUID.randomUUID()));
+        List<Beer> beers = beerController.listBeers();
+        System.out.println(beerController.getBeerByID(beers.getFirst().getId()));
+    }
+
+    @Test
+    void testlistBeers() {
+        log.debug("Test listBeers");
+        System.out.println(beerController.listBeers());
     }
 }
