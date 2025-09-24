2b6a0e4ea105b233257668a3190edfeac40b5843
Riccardo Corsi <riccardogcorsi@gmail.com>
2025-09-24 17:27:50 +0200

    CustomerControllerTest with MockMvc

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -2,15 +2,19 @@ package guru.springframework.spring6restmvc.controller;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,6 +22,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.model.Customer;
@@ -39,7 +45,15 @@ public class CustomerControllerTest {
     @MockitoBean  
     CustomerService customerService;
 
-    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+    CustomerServiceImpl customerServiceImpl;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        customerServiceImpl = new CustomerServiceImpl();
+    }
 
     @Test
     void listCustomersTest() throws Exception {
@@ -78,6 +92,25 @@ public class CustomerControllerTest {
         //assertEquals(myList.size(), 3);
     }
 
+    @Test
+    void testCreateNewCustomer() throws Exception {
+        Customer customerToSave = customerServiceImpl.listCustomers().get(0);
+        customerToSave.setId(null);
+        customerToSave.setVersion(1);
+        log.debug("test createNewCustomer ");
+        Customer savedCustomer = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("New Customer")
+                .version(customerToSave.getVersion())
+                .build();
 
+        given(customerService.saveNewCustomer(any(Customer.class))).willReturn(savedCustomer);
+        mockMvc.perform(post("/api/v1/customer")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(customerToSave)))  // context of the request
+            .andExpect(status().isCreated())                        // then
+            .andExpect(header().string("Location", "/api/v1/customer/" + savedCustomer.getId())); // check exact url of location url+id
+    }
 
 }
