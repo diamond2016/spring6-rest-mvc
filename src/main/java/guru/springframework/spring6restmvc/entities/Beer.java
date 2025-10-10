1ed7acf4878768e3de790fcccd3091c0b79ad354
Riccardo Corsi <riccardogcorsi@gmail.com>
2025-10-10 10:23:28 +0200

    implementned entities Beer and Customer and updated pom with JPA

---
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -0,0 +1,38 @@
+package guru.springframework.spring6restmvc.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+
+@Getter                     // @Data not recommended for entities
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Beer {
+    @Id
+    private UUID id;
+
+    @Version
+    private Integer version; //0,1...
+    
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
+
